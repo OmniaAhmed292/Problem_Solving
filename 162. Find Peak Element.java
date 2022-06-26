50b9d60dd6ceafc1bd783b1285499fc889db0486
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-06-26 14:47:00 +0200

    Create 162. Find Peak Element.java
    Adding a new solution

---
+++ 162. Find Peak Element.java
@@ -0,0 +1,40 @@
+//Status: Accepted
+class Solution {
+   public static int findPeakElement(int[] nums) {
+        //Get the middle start, middle and end indexes.
+        int start, middle, end,size;
+        start = 0;
+        end = nums.length - 1;
+        
+     
+        //initialize of variables middle and size
+        middle = (start + end) / 2;
+        size=end-start+1;
+        while(size>1) {
+            if (size== 2) { return (nums[start] > nums[end]) ? start:end;}
+            //Check if there's only one element left
+            if(middle==start && middle ==end){
+                return middle;
+            }
+            //Check if the middle index is bigger than its neighbours and if it's bigger return the index
+            if (nums[middle] > nums[middle + 1]) {
+                if (nums[middle] > nums[middle - 1]) return middle;
+                else {
+                    //middle is bigger than only the right neighbour so remove the right side of the array
+                    end = middle-1;
+                }
+            } else {
+                //the right neighbour is bigger than the middle so remove the left side of the array
+                start = middle+1;
+            }
+            //Update of variable middle and size
+            middle = (start + end) / 2;
+            size =size=end-start+1;
+
+
+        }
+        return middle;
+
+    }
+   
+}
