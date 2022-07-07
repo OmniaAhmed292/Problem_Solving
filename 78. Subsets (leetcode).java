48f2ddf96cb1cb0caeff4e9871cdbb2a56073a03
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-07-07 13:48:30 +0200

    Update 78. Subsets (leetcode).java

--- 78. Subsets (leetcode).java
+++ 78. Subsets (leetcode).java
@@ -2,22 +2,27 @@ import java.util.*;
 class Solution {
 public List<List> subsets(int[] nums) {
     List<List> sublist = new ArrayList<>();
-    int length = nums.length; //length of input array
-    double n = Math.pow(2.0,length)-1;   //get num of subsets as double
-    int len =(int) n;                    //num of subsets as integer
+    //length of input array
+    int length = nums.length;
+    //get num of subsets as double
+    double n = Math.pow(2.0,length)-1;  
+    //num of subsets as integer
+    int len =(int) n;                    
     for(int i=0;i<=len;i++){
         List list = new ArrayList<>();
         for(int j = 0 ;j<=length-1;j++){ //entering the array
             int bitshift = (1<<j);
-            if((i & bitshift) !=0){      //check set bit  to add in sublist or not
+            //check set bit  to add in sublist or not
+            if((i & bitshift) !=0){      
                 list.add(nums[j]);
-                                  }
-                                        }
+             }
+        }
         sublist.add(list);
-                      }
+    }
     return sublist;
-                                      }
-                 }
+}
+                 
+}
                  
                 
                 
