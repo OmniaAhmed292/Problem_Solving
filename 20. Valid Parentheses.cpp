e05ffbdaee1d175629cecaa6113c4b55f3df7e6d
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-11-08 12:45:26 +0200

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,44 @@
+/* https://leetcode.com/problems/valid-parentheses/submissions/ */
+
+class Solution {
+public:
+    bool isValid(string s) {
+        //Stack of Chars to hold elements of the String
+        stack<char> stack;
+      
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]== '[' ){
+                stack.push(s[i]); 
+            }
+            else if(s[i]==')' ||s[i]=='}' ||s[i]==']') {
+                //if the closing Parentheses is the start of the string return false
+                if(stack.empty()) return false;
+              //Matching each Parentheses with its closing
+                else if(s[i]==')' && stack.top()=='('){
+                    stack.pop(); 
+                    
+                }
+                else if(s[i]=='}' && stack.top()=='{'){
+                    stack.pop(); 
+                }
+                else if(s[i]==']' && stack.top()=='['){
+                    stack.pop(); 
+                }
+              //For invalid matching
+                else{
+                    
+                    return false;}
+            
+            }
+            //For invalid inputs
+            else {
+                return false;}
+                
+        
+
+            
+}
+//Return the condition of the stack because maybe some Parentheses aren't closed yet
+return stack.empty();
+ }
+};
