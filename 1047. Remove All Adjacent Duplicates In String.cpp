02accad3bd3275277a08c68fb21088401fa20456
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-11-08 20:17:14 +0200

    Create 1047. Remove All Adjacent Duplicates In String.cpp

---
+++ 1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,21 @@
+/* https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/*/
+
+class Solution {
+public:
+  
+    string removeDuplicates(string s) {
+      //Treating a string like a stack
+        string stack;
+        for(int i=0;i<s.length();i++){
+          
+            if(stack.empty()){stack.push_back(s[i]);}
+          
+            else if(stack.back()==s[i]){stack.pop_back();}
+          
+            else{stack.push_back(s[i]);
+                                  }
+        }
+        
+        return stack;
+    }
+};
