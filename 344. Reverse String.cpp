5ff103336b9cd060a30c70a4e6ec5ac89d7deaf2
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2022-11-13 12:47:51 +0200

    Update 344. Reverse String.cpp

--- 344. Reverse String.cpp
+++ 344. Reverse String.cpp
@@ -21,8 +21,31 @@ public:
     }
 };
 //ـــــــــــــــــــــــــــــــــــــــــــــــــ
-
+//Hacker rank question
 /* 
 2) Recursive method
 */
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
 
+string reverse_string(string s,int n)
+{
+if ((s.length()  <= 1)) {
+            return s;
+        }
+        string reversed = reverse_string(s.substr(1),n-1) + s.at(0);
+        return reversed;
+    
+}
+
+int main() {
+    string s;
+  	cin>>s;
+  	cout<<reverse_string(s,s.length())<<endl;
+    return 0;
+}
