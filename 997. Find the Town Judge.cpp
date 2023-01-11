b1b64737d4283e5f6736ee5b77502dbbe6ad22ea
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2023-01-11 13:06:29 +0200

    Create 997. Find the Town Judge.cpp

---
+++ 997. Find the Town Judge.cpp
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/find-the-town-judge/description/?envType=study-plan&id=data-structure-ii
+*/
+
+/*
+Easy code, uses Frequency array
+Better approach is to use in and out degrees but here I only use one vector for both.
+*/
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+      vector<int> freqarr(n+1,0);
+      for(int i=0;i<trust.size();i++){
+          freqarr[trust[i][0]]--;
+          freqarr[trust[i][1]]++;
+          
+      }
+      for(int j=1;j<=n;j++){
+          if(freqarr[j]==n-1) return j;
+      }
+      return -1;
+    }
+};
