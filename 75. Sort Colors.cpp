0387267eb3fca882cb72535bb6c2aa6373eeed30
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2023-03-30 20:46:11 +0200

    Three pointers approach

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -18,3 +18,29 @@ public:
         
     }
 };
+
+//Follow up: Could you come up with a one-pass algorithm using only constant extra space? 
+/* Three pointers approach one pass */ 
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int right=nums.size()-1;
+        int left= 0;
+        int i=0;
+        while(i<=right){
+            if(nums[i]==2){
+                std::swap(nums[right],nums[i]);
+                right--;
+                //if we swapped to the right we might need to swap to left again so can't increase pointer
+                continue;
+            }
+            else if(nums[i]==0){
+                std::swap(nums[left],nums[i]);
+                left++;
+            }
+            i++;
+            
+        }
+    }
+};
