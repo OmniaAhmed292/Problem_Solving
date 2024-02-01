874d1ce21d9898bc02625b845a7b3d6ebc293a2f
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-02-01 20:33:25 +0200

    Create 739. Daily Temperatures.java

---
+++ LeetCode Problems/Medium/739. Daily Temperatures.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/daily-temperatures/
+
+public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack= new Stack<>();
+        for(int i=0;i<temperatures.length;i++)
+        {
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()])
+            {
+                int index= stack.pop();
+                res[index]=i-index;
+            }
+            stack.push(i);
+        }
+        return res;
+
+    }
