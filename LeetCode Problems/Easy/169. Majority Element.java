2733765b45104929e4f613bbccdb7d5e313148c3
omnyaahmed292 <omnyaahmed292@gmail.com>
2024-02-12 19:03:57 +0200

    Create File for a solution and update another

--- LeetCode Problems/Easy/169. Majority Element.java
+++ LeetCode Problems/Easy/169. Majority Element.java
@@ -4,3 +4,16 @@ public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
     }
+//Moore voting algorithm
+//O(n) time O(1) space
+
+public int majorityElement(int[] nums) {
+       int count=0;
+       int candidate=0;
+       for(int num :nums){
+           if(count==0) candidate=num;
+           if(num==candidate) count++;
+           else count--;
+       }
+       return candidate; 
+    }
