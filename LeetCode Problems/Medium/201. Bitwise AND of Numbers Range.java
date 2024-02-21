f5cc5733748a7e4b283416ec473aaad500a9ee52
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-02-21 09:51:42 +0200

    Create 201. Bitwise AND of Numbers Range.java

---
+++ LeetCode Problems/Medium/201. Bitwise AND of Numbers Range.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/bitwise-and-of-numbers-range/
+public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+
+}
