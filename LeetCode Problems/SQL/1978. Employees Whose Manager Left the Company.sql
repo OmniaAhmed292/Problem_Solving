757e3a08a7534a74d8b707df2054693ce474ee04
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-03-02 18:53:07 +0200

    Create 1978. Employees Whose Manager Left the Company.sql

---
+++ LeetCode Problems/SQL/1978. Employees Whose Manager Left the Company.sql
@@ -0,0 +1,5 @@
+/*https://leetcode.com/problems/employees-whose-manager-left-the-company/description*/
+SELECT employee_id FROM Employees 
+WHERE salary< 30000
+AND  manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id ASC
