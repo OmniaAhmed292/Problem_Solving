bb2e443628eca376c1e73d7e429ec670fe024d58
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-03-10 08:47:39 +0200

    Update 2540. Minimum Common Value.java

--- LeetCode Problems/Easy/2540. Minimum Common Value.java
+++ LeetCode Problems/Easy/2540. Minimum Common Value.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/minimum-common-value
+//Two pointers solution
 public int getCommon(int[] nums1, int[] nums2) {
         int i = 0, j = 0;
         int res = -1;
@@ -13,4 +14,24 @@ public int getCommon(int[] nums1, int[] nums2) {
             }
         }
         return res;
+    }
+//Binary search solution 
+public int getCommon(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        for(int i=0;i<m;i++){
+            int left = 0;
+            int right = n-1;
+            while(left<=right){
+                int mid = left + (right-left)/2;
+                if(nums2[mid]==nums1[i]){
+                    return nums2[mid];
+                }else if(nums2[mid]>nums1[i]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+        }
+        return -1;
     }
