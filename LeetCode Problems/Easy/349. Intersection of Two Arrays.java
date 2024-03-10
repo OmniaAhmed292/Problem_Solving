5ceec906c30294dc271dec66b40ccc091d5e8367
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-03-10 08:52:11 +0200

    Add Binary search approach 349. Intersection of Two Arrays.java

--- LeetCode Problems/Easy/349. Intersection of Two Arrays.java
+++ LeetCode Problems/Easy/349. Intersection of Two Arrays.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/intersection-of-two-arrays
+//Two pointers approach
   public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
@@ -24,4 +25,36 @@
         }
         return result;
 
+    }
+
+//Binary search
+ public int[] intersection(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            if(i>0&&nums1[i]==nums1[i-1]){
+                continue;
+            }
+            int left = 0;
+            int right = n-1;
+            while(left<=right){
+                int mid = left + (right-left)/2;
+                if(nums2[mid]==nums1[i]){
+                    res.add(nums2[mid]);
+                    break;
+                }else if(nums2[mid]>nums1[i]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+        }
+        int[] result = new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            result[i] = res.get(i);
+        }
+        return result;
     }
