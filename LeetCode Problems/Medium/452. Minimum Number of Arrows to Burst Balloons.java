492b9a2089fac8a0086f8b689f86ccc767983fdf
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-03-18 13:06:37 +0200

    Create 452. Minimum Number of Arrows to Burst Balloons.java

---
+++ LeetCode Problems/Medium/452. Minimum Number of Arrows to Burst Balloons.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons
+public int findMinArrowShots(int[][] points) {
+        if(points.length == 0) return 0;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        int count = 1;
+        int end = points[0][1];
+        for(int i = 1; i < points.length; i++) {
+            if(points[i][0] > end) {
+                count++;
+                end = points[i][1];
+            }
+        }
+        return count;
+    }
