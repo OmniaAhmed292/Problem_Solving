f06bc26c25ca797bb54b463aed35935639a6622c
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-03-26 15:56:19 +0200

    Update 41. First Missing Positive.java

--- LeetCode Problems/Hard/41. First Missing Positive.java
+++ LeetCode Problems/Hard/41. First Missing Positive.java
@@ -12,3 +12,47 @@
         return n+1;
     }
 
+//Time complexity: O(n) Space complexity: O(n)
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        boolean[] present = new boolean[n+1];
+        for(int i=0;i<n;i++){
+            if(nums[i]>0 && nums[i]<=n){
+                present[nums[i]]=true;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(!present[i]){
+                return i;
+            }
+        }
+        return n+1;
+    }
+
+//Time complexity: O(n) Space complexity: O(1)
+public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        int ans=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0 && nums[i]<=n){
+                int correctPos=nums[i]-1;
+                //if the number is not at the correct position
+                if(nums[i]!=nums[correctPos]){
+                    //swap
+                    int temp=nums[i];
+                    nums[i]=nums[correctPos];
+                    nums[correctPos]=temp;
+                    i--;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(nums[i-1]!=i){
+                return i;
+            }
+        }
+        return n+1;
+    }
+
+
+
