ac9c0d42a0282ccdd908f6c5ecdbfd03893cb6f6
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-04-05 15:39:34 +0200

    Create 35. Search Insert Position.java

---
+++ LeetCode Problems/Easy/35. Search Insert Position.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/search-insert-position/description
+public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
