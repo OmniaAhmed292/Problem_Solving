1a84cc5eb4c0656b419089ada5fed6a81df0c00b
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-05-04 14:15:49 +0300

    Create 881. Boats to Save People.java

---
+++ LeetCode Problems/Medium/881. Boats to Save People.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/boats-to-save-people/description/
+public int numRescueBoats(int[] people, int limit) {
+     Arrays.sort(people);
+        int i = 0;
+        int j = people.length - 1;
+        int count = 0;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            count++;
+        }
+        return count;
+
+    }
