b80fcc7da034cc91903feff6ef2c7b3afed35a74
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2024-05-05 16:35:31 +0300

    Update and rename 237. Delete Node in a Linked List.cpp to 237. Delete Node in a Linked List.java

---
+++ LeetCode Problems/Medium/237. Delete Node in a Linked List.java
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+
+ public void deleteNode(ListNode node) {
+        node.val= node.next.val;
+        node.next=node.next.next;
+    }
