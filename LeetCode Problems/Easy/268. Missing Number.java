0e9252258390268c8767fbadaa4673c172aebc84
Omnia Ahmed <74054844+OmniaAhmed292@users.noreply.github.com>
2025-02-20 09:44:28 +0200

    Update 268. Missing Number.java

--- LeetCode Problems/Easy/268. Missing Number.java
+++ LeetCode Problems/Easy/268. Missing Number.java
@@ -7,3 +7,13 @@ public int missingNumber(int[] nums) {
         }
         return sum;
     }
+
+   public int missingNumber(int[] nums) {
+        int realSum = nums.length * (nums.length+1)/2;
+        int Sum=0;
+        for(int i=0;i<nums.length;i++){
+            Sum+=nums[i];
+        }
+        return realSum-Sum;
+
+    }
